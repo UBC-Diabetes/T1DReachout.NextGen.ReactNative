2ceee082ba89ee912de1a1138cb62aedb9bcf5db
Timothy Roy <timothy.roy@pm.me>
2025-07-15 08:48:38 -0700

    Update View Profile

--- app/views/RoomInfoView/Direct.tsx
+++ app/views/RoomInfoView/Direct.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-import I18n from '../../i18n';
 import { useTheme } from '../../theme';
-import CustomFields from './CustomFields';
-import Timezone from './Timezone';
 import styles from './styles';
 import { IUser } from '../../definitions';
 
@@ -23,9 +20,77 @@ const Direct = ({ roomUser }: { roomUser: IUser }): React.ReactElement => {
 	
 	return (
 		<>
-			{/* Basic Info Section - Following ProfileView order */}
+			{/* Main Info Section - White background extending all the way down */}
 			<View style={[styles.infoSection, { backgroundColor: colors.nextGenSurface }]}>
-				{/* Email - if available */}
+				{/* T1D Since - moved to top */}
+				{t1dSince && (
+					<View style={styles.infoRow}>
+						<Text style={[styles.infoLabel, { color: colors.nextGenText }]}>T1D Since</Text>
+						<Text style={[styles.infoValue, { color: colors.nextGenText }]}>{t1dSince}</Text>
+					</View>
+				)}
+				
+				{/* Glucose Monitoring Method - moved to top */}
+				{glucoseMethod && (
+					<View style={styles.infoRow}>
+						<Text style={[styles.infoLabel, { color: colors.nextGenText }]}>Glucose Monitoring Method</Text>
+						<Text style={[styles.infoValue, { color: colors.nextGenText }]}>{glucoseMethod}</Text>
+					</View>
+				)}
+				
+				{/* Insulin Delivery Method - moved to top */}
+				{insulinMethod && (
+					<View style={styles.infoRow}>
+						<Text style={[styles.infoLabel, { color: colors.nextGenText }]}>Insulin Delivery Method</Text>
+						<Text style={[styles.infoValue, { color: colors.nextGenText }]}>{insulinMethod}</Text>
+					</View>
+				)}
+				
+				{/* About (formerly Bio) - moved after device fields */}
+				{bio && (
+					<View style={styles.fieldRow}>
+						<Text style={[styles.fieldLabel, { color: colors.nextGenText }]}>About</Text>
+						<Text style={[styles.fieldValue, { color: colors.nextGenText }]}>{bio}</Text>
+					</View>
+				)}
+				
+				{/* Roles - moved after device fields */}
+				{roomUser.roles?.length && (
+					<View style={styles.fieldRow}>
+						<Text style={[styles.fieldLabel, { color: colors.nextGenText }]}>Roles</Text>
+						<View style={styles.rolesContainer}>
+							{roomUser.roles.map(role =>
+								role ? (
+									<View
+										style={[styles.roleBadge, { backgroundColor: colors.nextGenLightGray }]}
+										key={role}
+										testID={`user-role-${role.replace(/ /g, '-')}`}
+									>
+										<Text style={[styles.role, { color: colors.fontTitlesLabels }]}>{role}</Text>
+									</View>
+								) : null
+							)}
+						</View>
+					</View>
+				)}
+				
+				{/* Name */}
+				{roomUser.name && (
+					<View style={styles.fieldRow}>
+						<Text style={[styles.fieldLabel, { color: colors.nextGenText }]}>Name</Text>
+						<Text style={[styles.fieldValue, { color: colors.nextGenText }]}>{roomUser.name}</Text>
+					</View>
+				)}
+				
+				{/* Username */}
+				{roomUser.username && (
+					<View style={styles.fieldRow}>
+						<Text style={[styles.fieldLabel, { color: colors.nextGenText }]}>Username</Text>
+						<Text style={[styles.fieldValue, { color: colors.nextGenText }]}>{roomUser.username}</Text>
+					</View>
+				)}
+				
+				{/* Email */}
 				{roomUser.emails && roomUser.emails.length > 0 && (
 					<View style={styles.fieldRow}>
 						<Text style={[styles.fieldLabel, { color: colors.nextGenText }]}>Email</Text>
@@ -56,72 +121,7 @@ const Direct = ({ roomUser }: { roomUser: IUser }): React.ReactElement => {
 						<Text style={[styles.fieldValue, { color: colors.nextGenText }]}>{pronouns}</Text>
 					</View>
 				)}
-				
-				{/* T1D Since */}
-				{t1dSince && (
-					<View style={styles.fieldRow}>
-						<Text style={[styles.fieldLabel, { color: colors.nextGenText }]}>T1D Since</Text>
-						<Text style={[styles.fieldValue, { color: colors.nextGenText }]}>{t1dSince}</Text>
-					</View>
-				)}
-				
-				{/* Glucose Monitoring Method */}
-				{glucoseMethod && (
-					<View style={styles.fieldRow}>
-						<Text style={[styles.fieldLabel, { color: colors.nextGenText }]}>Glucose Monitoring Method</Text>
-						<Text style={[styles.fieldValue, { color: colors.nextGenText }]}>{glucoseMethod}</Text>
-					</View>
-				)}
-				
-				{/* Insulin Delivery Method */}
-				{insulinMethod && (
-					<View style={styles.fieldRow}>
-						<Text style={[styles.fieldLabel, { color: colors.nextGenText }]}>Insulin Delivery Method</Text>
-						<Text style={[styles.fieldValue, { color: colors.nextGenText }]}>{insulinMethod}</Text>
-					</View>
-				)}
-				
-				{/* Timezone */}
-				{roomUser.utcOffset !== undefined && (
-					<View style={styles.fieldRow}>
-						<Text style={[styles.fieldLabel, { color: colors.nextGenText }]}>Timezone</Text>
-						<Text style={[styles.fieldValue, { color: colors.nextGenText }]}>
-							{roomUser.utcOffset > 0 ? '+' : ''}{roomUser.utcOffset}
-						</Text>
-					</View>
-				)}
 			</View>
-
-			{/* Bio Section - Gray Background */}
-			{bio && (
-				<View style={[styles.aboutSection, { backgroundColor: colors.nextGenBackground }]}>
-					<Text style={[styles.aboutHeader, { color: colors.nextGenText }]}>Bio</Text>
-					<Text style={[styles.aboutText, { color: colors.nextGenText }]}>{bio}</Text>
-				</View>
-			)}
-
-			{/* Roles Section - Gray Background */}
-			{roomUser.roles?.length && (
-				<View style={[styles.aboutSection, { backgroundColor: colors.nextGenBackground }]}>
-					<Text style={[styles.aboutHeader, { color: colors.nextGenText }]}>Roles</Text>
-					<View style={styles.rolesContainer}>
-						{roomUser.roles.map(role =>
-							role ? (
-								<View
-									style={[styles.roleBadge, { backgroundColor: colors.nextGenSecondary }]}
-									key={role}
-									testID={`user-role-${role.replace(/ /g, '-')}`}
-								>
-									<Text style={[styles.role, { color: colors.fontTitlesLabels }]}>{role}</Text>
-								</View>
-							) : null
-						)}
-					</View>
-				</View>
-			)}
-
-			{/* Additional Custom Fields */}
-			<CustomFields customFields={roomUser.customFields} />
 		</>
 	);
 };
