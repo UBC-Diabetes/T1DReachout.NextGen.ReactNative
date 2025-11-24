b45fc869349620af40bfcb3cc4d3a9ca0795ed70
Timothy Roy <timothy.roy@pm.me>
2025-11-24 13:41:01 -0600

    Make Discussion Boards saved posts use existent helper function

--- app/views/DiscussionBoard/DiscussionHomeView/index.tsx
+++ app/views/DiscussionBoard/DiscussionHomeView/index.tsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import { useIsFocused, useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import * as HeaderButton from '../../../containers/HeaderButton';
-import { MESSAGE_TYPE_ANY_LOAD, SortBy, themes } from '../../../lib/constants';
+import { SortBy, themes } from '../../../lib/constants';
 import { withTheme } from '../../../theme';
 import { IApplicationState } from '../../../definitions';
 import DiscussionBoardCard from '../Components/DiscussionBoardCard';
@@ -12,13 +12,10 @@ import SavedPostCard from '../Components/SavedPostCard';
 import Header from '../Components/Header';
 import { DiscussionTabs } from './interaces';
 import makeStyles from './styles';
-import { messageTypesToRemove } from '../data';
 import { getRoomAvatar, isGroupChat } from '../../../lib/methods/helpers';
-import { loadMissedMessages } from '../../../lib/methods';
-import moment from 'moment';
-import { handleStar } from '../helpers';
 import { goRoom } from '../../../lib/methods/helpers/goRoom';
 import * as Services from '../../../lib/services/restApi';
+import { observeSavedPosts } from '../../HomeView/savedPostsHelpers';
 
 const VIRTUAL_HUDDLE = {
 	ROOM_RID: 'jRXA42HyPKpjAmZpX'
@@ -62,12 +59,6 @@ const DiscussionHomeView: React.FC = ({ route, theme }) => {
 			});
 		}
 	});
-
-	useEffect(() => {
-		if (isFocused && selectedTab === DiscussionTabs.SAVED_POSTS) {
-			getSavedChat();
-		}
-	}, [isFocused]);
 
 	useEffect(() => {
 		const getPublicChannels = async () => {
@@ -114,43 +105,18 @@ const DiscussionHomeView: React.FC = ({ route, theme }) => {
 		}
 	}, [isFocused, selectedTab, sortBy]);
 
-	const getSavedChat = async () => {
-		const messagesObservable = db.get('messages').query(Q.where('starred', true), Q.sortBy('ts', Q.desc), Q.skip(0)).observe();
-
-		messagesObservable?.subscribe(messages => {
-			// filter out messages
-			messages = messages.filter(m => {
-				return !(MESSAGE_TYPE_ANY_LOAD.includes(m.t) || messageTypesToRemove.includes(m.t));
-			});
-
-			const formattedData = messages.map(m => {
-				let object = { ...m };
-				try {
-					if (m?._raw?.u?.length && m._raw.u.length > 0 && m._raw.u !== '[]') {
-						object._raw.u = JSON.parse(m._raw.u);
-					}
-					if (m?._raw?.attachments?.length && m._raw.attachments.length > 0) {
-						object._raw.attachments = JSON.parse(m._raw.attachments);
-					}
-					if (m?._raw?.replies?.length && m._raw.replies.length > 0 && m._raw.replies !== '[]') {
-						object._raw.replies = JSON.parse(m._raw.replies);
-					}
-					if (m?._raw?.reactions?.length && m._raw.reactions.length > 0 && m._raw.reactions !== '[]') {
-						object._raw.reactions = JSON.parse(m._raw.reactions);
-					}
-				} catch (error) {}
-
-				return object;
-			});
-
-			setStarredPosts(formattedData);
-		});
-	};
-
-	// get starred posts
+	// Subscribe to saved posts updates when Saved Posts tab is active
 	useEffect(() => {
 		if (selectedTab === DiscussionTabs.SAVED_POSTS) {
-			getSavedChat();
+			const subscription = observeSavedPosts(999, posts => {
+				setStarredPosts(posts);
+			});
+
+			return () => {
+				if (subscription?.unsubscribe) {
+					subscription.unsubscribe();
+				}
+			};
 		}
 	}, [selectedTab]);
 
@@ -191,13 +157,7 @@ const DiscussionHomeView: React.FC = ({ route, theme }) => {
 				{selectedTab === DiscussionTabs.SAVED_POSTS && (
 					<FlatList
 						data={starredPosts}
-						renderItem={({ item }) => (
-							<SavedPostCard
-								post={item}
-								theme={theme}
-								server={server}
-							/>
-						)}
+						renderItem={({ item }) => <SavedPostCard post={item} theme={theme} server={server} />}
 						keyExtractor={item => item._raw.id}
 						style={{ paddingHorizontal: 20, paddingVertical: 4, marginBottom: 32 }}
 						ListFooterComponent={<View style={styles.footer} />}
